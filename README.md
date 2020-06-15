302d42964ddd6ba4be028d78377850434c6a99bc
Jan Konopka <janfrederik.konopka@gmail.com>
2020-06-15 15:49:47 +0200

    better handling of whitespace (display correctly, limit to 2 consecutive newlines)

--- README.md
+++ README.md
@@ -24,5 +24,6 @@ TODO:
 
 </td>
 </tr>
+</table>
 
 <!-- https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-ubuntu-18-04 -->
--- create.php
+++ create.php
@@ -4,12 +4,16 @@
 
 $content = file_get_contents('php://input');
 
-// disallow empty or all-space posts
+// maximum post length 140 chars (560 bytes)
+$content = mb_substr($content, 0, 140);
+
+// limit to 2 consecutive newlines
+$content = preg_replace( "/\n\s*\n/", "\n\n", $content );
+
+// disallow empty or all-space posts, trim superfluous space
 $content = trim($content);
 if ($content == "")
     return;
-
-$content = mb_substr($content, 0, 140);
 
 $con = new mysqli("mysql", "newuser", "user_password", "social");
 
